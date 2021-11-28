c4247bd7e4767d86a6d131d4212d4f06af728993
ddisakov <dmisakovn@gmail.com>
2021-11-28 21:11:31 +0300

    5

---
+++ hello.py
@@ -0,0 +1,5 @@
+def app(environ, start_response):
+    """Simplest possible application object"""
+    res = '\n'.join(environ['QUERY_STRING'].split('&'))
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return [bytes(res, encoding='utf-8')]
